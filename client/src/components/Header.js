1206fb0b263131d449c3addc29906aa4ac9de6db
mnedelko <mnedelko@gmail.com>
2018-09-22 16:29:07 -0700

    Added billing changes to application

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments  from './Payments';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth){
+            case null:
+                return;
+            case false:
+                return (
+                    <li><a href="/auth/google">Login With google</a></li>
+                );
+            default:
+                return [
+                <li key="1"><Payments/></li>,    
+                <li hey="3" style={{margin: '0 10px'}}><a>Credit: {this.props.auth.credits}</a></li>,
+                <li key="2"><a href="/api/logout">Logout</a></li>
+            ];
+        }
+    }
     render() {
         return (            
             <nav>
                 <div className="nav-wrapper">
-                    <a href="/auth/google" className="left brand-logo">Emaily</a>
+                    <Link 
+                        to={this.props.auth ? '/surveys' : '/'} 
+                        className="left brand-logo"
+                        >
+                        Emaily
+                    </Link>
                     <ul className="right">
-                        <li>
-                            <a>Login with Google</a>
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
@@ -17,4 +39,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }){
+    return{ auth };
+}
+
+export default connect(mapStateToProps) (Header);
